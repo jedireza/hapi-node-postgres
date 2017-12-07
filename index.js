f7d91067aa93b5b60eb7c566aec18a170059d136
EADEV <eadev@EADEVs-MacBook-Pro.local>
2017-12-07 13:45:58 +0000

    Hapi 17 Migration

--- index.js
+++ index.js
@@ -8,52 +8,42 @@ const DEFAULTS = {
     connectionString: undefined,
     native: false,
     attach: 'onPreHandler',
-    detach: 'tail'
+    detach: 'response'
 };
 
+exports.plugin = {  
+    register: (server, options) => {
 
-exports.register = function (server, options, next) {
-
-    const config = Hoek.applyToDefaults(DEFAULTS, options);
-
-    if (config.native) {
-        Pg = require('pg').native;
-    }
-
-    server.expose('connect', Pg.connect.bind(Pg, config.connectionString));
-
-    server.ext(config.attach, (request, reply) => {
-
-        Pg.connect(config.connectionString, (err, client, done) => {
-
-            if (err) {
-                reply(err);
-                return;
-            }
-
-            request.pg = {
-                client,
-                done,
-                kill: false
-            };
-
-            reply.continue();
-        });
-    });
-
-
-    server.on(config.detach, (request, err) => {
-
-        if (request.pg) {
-            request.pg.done(request.pg.kill);
+        const config = Hoek.applyToDefaults(DEFAULTS, options);
+    
+        if (config.native) {
+            Pg = require('pg').native;
         }
-    });
+    
+        server.expose('connect', Pg.connect.bind(Pg, config.connectionString));
+    
+        server.ext(config.attach, (request, h) => {
+            return Pg
+                .connect(config.connectionString)
+                .then(client => {
+                    
+                    request.pg = {
+                        client,
+                        done: client.release,
+                        kill: false
+                    };
+                    
+                    return h.continue
 
+                });
+        });
 
-    next();
-};
-
-
-exports.register.attributes = {
+        server.events.on(config.detach, (request, err) => {
+    
+            if (request.pg) {
+                request.pg.done(request.pg.kill);
+            }
+        });
+    },
     pkg: require('./package.json')
-};
+}
